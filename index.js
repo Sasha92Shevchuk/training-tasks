58c85f12706d24a06f29342cfeba11119845b53e
Alex <ol.shevchuk92@gmail.com>
2023-08-09 20:43:56 +0300

    task sorted arr

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-function createPhoneNumber(numbers) {
-  let firstPart = [];
-  let secondPart = [];
-  let lastPart = [];
-  firstPart.push("(");
-  for (let index = 0; index < numbers.length; index++) {
-    if (index <= 2) {
-      firstPart.push(numbers[index]);
-    } else if (index > 2 && index < 6) {
-      secondPart.push(numbers[index]);
-    } else {
-      lastPart.push(numbers[index]);
-    }
-  }
-  firstPart.push(")");
+// function createPhoneNumber(numbers) {
+//   let firstPart = [];
+//   let secondPart = [];
+//   let lastPart = [];
+//   firstPart.push("(");
+//   for (let index = 0; index < numbers.length; index++) {
+//     if (index <= 2) {
+//       firstPart.push(numbers[index]);
+//     } else if (index > 2 && index < 6) {
+//       secondPart.push(numbers[index]);
+//     } else {
+//       lastPart.push(numbers[index]);
+//     }
+//   }
+//   firstPart.push(")");
 
-  return firstPart.concat(" ", secondPart.concat("-", lastPart)).join("");
+//   return firstPart.concat(" ", secondPart.concat("-", lastPart)).join("");
+// }
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // => returns "(123) 456-7890"
+
+//===============>
+
+//Write an algorithm that takes an array and
+// moves all of the zeros to the end, preserving the order of the other elements.
+
+function moveZeros(arr) {
+  const zeroArr = [];
+  const newArr = [];
+  arr.forEach((element) => {
+    element === 0 ? zeroArr.push(element) : newArr.push(element);
+  });
+
+  return newArr.concat(zeroArr);
 }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // => returns "(123) 456-7890"
+moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]); // returns[false,1,1,2,1,3,"a",0,0]
